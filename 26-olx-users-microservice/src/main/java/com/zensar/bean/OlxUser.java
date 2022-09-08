4263622abfd4ef0a06c852ea750c59d6bcf07b8f
Kuchivenkatakesava <111448196+Kuchivenkatakesava@users.noreply.github.com>
2022-09-08 12:05:04 +0530

    Add files via upload

---
+++ 26-olx-users-microservice/src/main/java/com/zensar/bean/OlxUser.java
@@ -0,0 +1,72 @@
+package com.zensar.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class OlxUser {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String username;
+	private String password;
+	private String roles;
+	private String active;
+	private String firstName;
+	private String lastName;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRoles() {
+		return roles;
+	}
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+	public String getActive() {
+		return active;
+	}
+	public void setActive(String active) {
+		this.active = active;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	@Override
+	public String toString() {
+		return "OlxUser [id=" + id + ", username=" + username + ", password=" + password + ", roles=" + roles
+				+ ", active=" + active + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+
+}
---
+++ 33-docker2/README.txt
@@ -0,0 +1,23 @@
+Docker
+ 
+1) It is a container management service.
+2) The whole idea of docker is to be able to ship applications along with their enviornment into images.
+   Which then can be run anywhere. (Even on Cloud platforms e.g. AWS)
+
+ 3) Docker Image : - In Docker image acts as set of instructions to build container. (Like template)
+
+ 4) Docker container : It is running instance of a image.
+
+
+ e.g. To Run Java application
+
+ jdk + application + OS = Image
+
+ Provided - Docker in installed on that Machine.
+
+ ----------------------------------
+
+ There are commands to manage images and container.
+
+ commands for managing image:-
+ 1)docker image is  This command shows all the images available locally
